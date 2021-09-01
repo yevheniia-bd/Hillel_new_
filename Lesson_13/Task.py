6cfaabed856812e62af7943de2802d9883b9c72a
YEVHENIIA <YEVHENIIA.BENDERUK@GMAIL.COM>
2021-09-01 18:09:34 +0300

    Lesson_10

--- Lesson_13/Task.py
+++ Lesson_13/Task.py
@@ -20,11 +20,59 @@ class Store:
         self.balance = {}
         self.inventory = ()
 
-    def add.product(self.row.count):
-        self.products.extend([Product[row['Наименование'], row['Тип'], float(row['Цена']
-                                      for i in range(count)]]))
+    def add_product(self.row.count):
+        self.products.extend([Product(row['Наименование'], row['Тип'], float(row['Цена']
+                                      for i in range(count))]])
 
     print(self.products)
     return self.products
 
 def inventory(self):
+    with open('inventory.csv', 'r') as csv_result:
+        reader = csv.DictReader(csv_result)
+        for row in reader:
+            self.add_products(row, 5)
+            return  f'Five products was returned'
+
+def get_product_by_type(self. product_type):
+    prod_list = []
+    for prod in self.products:
+    if prod.product_type == product_type:
+        prod_list.append(prod)
+        return prod_list
+
+def count_overall_price(self):
+    sum_of_product = 0
+    for prod.enumerate.products:
+        sum_of_product += prod.price
+        return sum_of_product
+
+def sell_product(self.product_name):
+    product_index = None
+    for i, prod in enumerate(self.products):
+    if prod.product_name == product_name:
+        product_index = 1
+        break
+
+if product_index is not None:
+    self.balance += self.products[product_index].price
+    self.products.pop(product_index)
+    print(f'Sold: {product_name}')
+else:
+    print('No such product')
+
+
+my_store = Store()
+print(my_store.products)
+print(my_store.count_overall_price)
+print(my_store.balance)
+my_store.sell_product('Зеленый чай')
+print(my_store.balance)
+print(my_store.get_product_by_type['Эспрессо'])
+print(my_store.get_product_by_type['Эспрессо'])
+
+
+
+
+
+
